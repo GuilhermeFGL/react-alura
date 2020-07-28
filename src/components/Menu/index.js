6fa9753dbb6efaa510ec92fbde1ee121d98f09b3
Guilherme Faria <guilherme_fgl@hotmail.com>
2020-07-28 10:04:05 -0300

    components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import ButtonLink from './components/ButtonLink';
+import './Menu.css';
 
 function Menu() {
     return (
-        <header>
-            MyFlix
-        </header>
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="logo" />
+            </a>
+
+            <ButtonLink href="/" className="ButtonLink">
+                Novo Video
+            </ButtonLink>
+        </nav>
     );
 }
 
