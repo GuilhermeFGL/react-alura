7a322f917f38ae664fbf5ccce09b3e1066a08185
Guilherme Faria <guilherme_fgl@hotmail.com>
2020-07-28 19:14:16 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 import './index.css';
-import App from './App';
+import CadastroVideo from './pages/cadastro/Video';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={() => (<div>Página 404</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[4].videos[2].titulo}
+        url={dadosIniciais.categorias[4].videos[2].url}
+        videoDescription={dadosIniciais.categorias[4].videos[2].videoDescription} />
+
+      <Carousel category={dadosIniciais.categorias[0]}/>
+      <Carousel category={dadosIniciais.categorias[1]} />
+      <Carousel category={dadosIniciais.categorias[2]} />
+      <Carousel category={dadosIniciais.categorias[3]} />
+      <Carousel category={dadosIniciais.categorias[4]} />
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
