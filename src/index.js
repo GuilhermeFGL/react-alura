be9039317526706d388112d3dc178c88c5966482
Guilherme Faria <guilherme_fgl@hotmail.com>
2020-07-29 17:17:35 -0300

    react slider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
 import './index.css';
+import CadastroCategoria from './pages/cadastro/Categorias';
 import CadastroVideo from './pages/cadastro/Video';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={() => (<div>Página 404</div>)} />
     </Switch>
   </BrowserRouter>,
